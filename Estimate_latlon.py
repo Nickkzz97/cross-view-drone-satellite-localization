94c6d55538aa5efd5f9ae105ac2d9f2c6fc78bb9
Anand Patel <sahoonicky@gmail.com>
2026-01-19 17:05:47 +0530

    Adding super point cross image stitching files

---
+++ Estimate_latlon.py
@@ -0,0 +1,166 @@
+import os
+import cv2
+import torch
+import rasterio
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import radians, sin, cos, sqrt, atan2
+from sklearn.model_selection import train_test_split
+from pyproj import Transformer
+from tqdm import tqdm
+from pathlib import Path
+import time
+from SuperGluePretrainedNetwork.models.superpoint import SuperPoint
+from SuperGluePretrainedNetwork.models.superglue import SuperGlue
+from utils import *
+
+
+with rasterio.open("task_cv_model/map.tif") as src:
+    src_crs = src.crs
+    sat_img = src.read([1,2,3]).transpose(1,2,0)
+    geo_transform = src.transform
+
+to_wgs84 = Transformer.from_crs(
+    src_crs, "EPSG:4326", always_xy=True
+)
+
+gt = pd.read_csv("task_cv_model/train_data/ground_truth.csv")
+gt = gt.sort_values("timestamp").reset_index(drop=True)
+
+samples = []
+
+for name in os.listdir("task_cv_model/train_data/drone_images"):
+    ts = float(os.path.splitext(name)[0])
+    gt_row = align_timestamp(ts, gt)
+
+    samples.append({
+        "path": os.path.join("task_cv_model/train_data/drone_images", name),
+        "timestamp": ts,
+        "lat": gt_row.latitude,
+        "lon": gt_row.longitude
+    })
+ 
+def evaluate(dataset, desc="Evaluating", csv_path=None):
+    errors = []
+    success = 0
+    times = []   # ⬅ store per-image time
+
+    tp_5m = tp_20m = 0
+    fn_5m = fn_20m = 0
+
+    total = len(dataset)
+
+    for i,sample in enumerate(tqdm(dataset, desc=desc, total=len(dataset))):
+        drone = cv2.imread(sample["path"])
+        t0 = time.time()
+        if i %50 == 0: pred = align_drone_to_satellite(drone, sat_img, sample["path"],i, vis = True)
+        pred = align_drone_to_satellite(drone, sat_img, sample["path"], i,vis = False)
+        t1 = time.time()
+
+        times.append(t1 - t0)
+        basename = Path(sample["path"]).stem
+        if pred is None:
+            record = {
+                "image": basename,
+                "status": "failed",
+                "pred_lat": None,
+                "pred_lon": None,
+                "gt_lat": sample["lat"],
+                "gt_lon": sample["lon"],
+                "error_m": None
+            }
+        else:
+
+            print('dtype pred, gt:', type(pred[0]),type(pred[1]),type(sample["lat"]))
+            pred_lat = float(pred[0])
+            pred_lon = float(pred[1])
+            gt_lat = float(sample["lat"])
+            gt_lon = float(sample["lon"])
+            err = haversine(
+                pred_lat, pred_lon,
+                gt_lat, gt_lon)
+            errors.append(err)
+            if err <= 5:
+                tp_5m += 1
+            else:
+                fn_5m += 1
+
+            if err <= 20:
+                tp_20m += 1
+            else:
+                fn_20m += 1
+            success += 1
+
+            record = {
+                "image": basename,
+                "status": "success",
+                "pred_lat": pred[0],
+                "pred_lon": pred[1],
+                "gt_lat": sample["lat"],
+                "gt_lon": sample["lon"],
+                "error_m": err
+            }
+
+            print(record)
+
+        # 🔹 APPEND RECORD IMMEDIATELY
+        if csv_path is not None:
+            pd.DataFrame([record]).to_csv(
+                csv_path,
+                mode="a",
+                header=False,
+                index=False
+            )
+
+    errors = np.array(errors)
+    times = np.array(times)
+
+    # --- Statistics ---
+    mean_error = np.mean(errors)
+    median_error = np.median(errors)
+    rmse_error = np.sqrt(np.mean(errors ** 2))
+    max_error = np.max(errors)
+
+    err_std = np.std(errors)
+    err_var = np.var(errors)
+
+    recall_5m = 100 * tp_5m / total
+    recall_20m = 100 * tp_20m / total
+
+    # avg_inliers = np.mean(inlier_counts)
+
+    avg_time = times.mean()
+    # --- Print ---
+    print(f"Error Std Dev : {err_std:.2f} m")
+    print(f"Error Variance: {err_var:.2f} m^2")
+
+    print(f"Recall @ 5m   : {recall_5m:.2f}%")
+    print(f"Recall @ 20m  : {recall_20m:.2f}%")
+
+    print(f"Mean error    : {mean_error:.2f} m")
+    print(f"Median error  : {median_error:.2f} m")
+    print(f"RMSE          : {rmse_error:.2f} m")
+    print(f"Max error     : {max_error:.2f} m")
+
+    # print(f"Average Inliers (proxy): {avg_inliers:.2f}")
+    print(f"Average time per image : {avg_time:.3f} s")
+
+    return {
+        "mean": mean_error,
+        "median": median_error,
+        "rmse": rmse_error,
+        "max": max_error,
+        "recall_5m": recall_5m,
+        "recall_20m": recall_20m,
+        # "avg_inliers": avg_inliers,
+        "avg_time_sec": avg_time,
+        "num_samples": success
+    }
+
+if __name__ == "__main__":
+    train_metrics = evaluate(
+    samples,
+    desc="Train set",
+    csv_path="superpoint_prediction.csv"
+)
--- utils.py
+++ utils.py
@@ -1,310 +1,328 @@
+import torch
 import cv2
 import os
-import rasterio
 import numpy as np
+from matplotlib import pyplot as plt
 import rasterio
-import time
-from rasterio.transform import xy
 from pyproj import Transformer
+from SuperGluePretrainedNetwork.models.superpoint import SuperPoint
+from SuperGluePretrainedNetwork.models.superglue import SuperGlue
 
-def load_satellite_map(tif_path):
-    dataset = rasterio.open(tif_path)
-    
-    # Read image (C, H, W) → (H, W, C)
-    image = dataset.read()
-    image = np.transpose(image, (1, 2, 0))
-    
-    transform = dataset.transform      # pixel ↔ geo transform
-    crs = dataset.crs                  # coordinate reference system
-    
-    return image, transform, crs
+# Initialize SuperPoint and SuperGlue models
+sp_config = {
+    "descriptor_dim": 256,
+    "nms_radius": 4,
+    "keypoint_threshold": 0.001,
+    "max_keypoints": 2048,
+    "remove_borders": 4
+}
+sg_config = {
+    "weights": "outdoor",
+    "sinkhorn_iterations": 20,
+    "match_threshold": 0.2
+}
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print('device:', device)
+superpoint = SuperPoint(sp_config).to(device).eval()
+superglue = SuperGlue(sg_config).to(device).eval()
 
-def load_drone_images(folder):
-    images = {}
-    for fname in os.listdir(folder):
-        if fname.endswith(('.png', '.jpg', '.jpeg')):
-            path = os.path.join(folder, fname)
-            img = cv2.imread(path)
-            images[fname] = img
-    return images
+def align_timestamp(img_ts, gt_df):
+    idx = np.argmin(np.abs(gt_df["timestamp"].values - img_ts))
+    return gt_df.iloc[idx]
 
-def preprocess_image(img, size=(512, 512)):
+def extract_superpoint(img, superpoint=superpoint):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # resized = cv2.resize(gray, size)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    return blurred
+    inp = torch.from_numpy(gray/255.).float()[None,None].to(device)
 
-def preprocess_drone_images(drone_images):
-    processed = {}
-    for name, img in drone_images.items():
-        processed[name] = preprocess_image(img)
-    return processed
+    with torch.no_grad():
+        out = superpoint({"image": inp})
 
-def satellite_to_gray(sat_img):
-    """
-    sat_img: (H, W, C) GeoTIFF image
-    """
-    # If more than 3 bands (common in satellite images)
-    if sat_img.shape[2] >= 3:
-        rgb = sat_img[:, :, :3].astype(np.uint8)
-        gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-    else:
-        gray = sat_img[:, :, 0]
-
-    return gray
-
-def normalize_uint8(img):
-    img = img.astype(np.float32)
-    img = 255 * (img - img.min()) / (img.max() - img.min() + 1e-6)
-    return img.astype(np.uint8)
-
-# functions
-def tile_image(img, tile_size=512, stride=256):
-    tiles = []
-    h, w = img.shape
-    for y in range(0, h - tile_size, stride):
-        for x in range(0, w - tile_size, stride):
-            tiles.append({
-                "tile": img[y:y+tile_size, x:x+tile_size],
-                "offset": (x, y)
-            })
-    return tiles
-
-# def extract_orb(img):
-#     return orb.detectAndCompute(img, None)
-
-# def match_features(des_d, des_s, ratio=0.75):
-#     matches = bf.knnMatch(des_d, des_s, k=2)
-#     return [m for m,n in matches if m.distance < ratio * n.distance]
-
-sift = cv2.SIFT_create(nfeatures=4000)
-def extract_sift(img):
-    return sift.detectAndCompute(img, None)
-
-def ratio_test(matches, ratio=0.75):
-    good = []
-    for m, n in matches:
-        if m.distance < ratio * n.distance:
-            good.append(m)
-    return good
-
-def match_keypoints(desc_1, desc_2, ratio=0.85):
-    FLANN_INDEX_KDTREE = 1  
-
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    desc_1 = np.asarray(desc_1, np.float32)
-    desc_2 = np.asarray(desc_2, np.float32)
-
-    matches = flann.knnMatch(desc_1, desc_2, k=2)
-    good = ratio_test(matches, ratio)
-    return matches, good
-
-def ransac_verify(kp_d, kp_s, matches):
-    if len(matches) < 10:
-        return None, 0
-
-    src = np.float32([kp_d[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
-    dst = np.float32([kp_s[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
-
-    H, mask = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
-    inliers = int(mask.sum()) if mask is not None else 0
-    return H, inliers
-
-def ransac_homography(kp_d, kp_s, matches):
-    if len(matches) < 10:
-        return None, 0
-
-    src = np.float32([kp_d[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
-    dst = np.float32([kp_s[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
-
-    H, mask = cv2.findHomography(
-        src, dst,
-        cv2.RANSAC,
-        ransacReprojThreshold=6
+    return (
+        out["keypoints"][0],
+        out["descriptors"][0],
+        out["scores"][0]
     )
 
-    inliers = int(mask.sum()) if mask is not None else 0
-    
-    if inliers<8:
-        return None, inliers
-    return H, mask.ravel(),inliers
+def superglue_match(img1, img2):
+    k1, d1, s1 = extract_superpoint(img1)
+    k2, d2, s2 = extract_superpoint(img2)
 
-# def ransac_homography(kp_d, kp_s, matches, thresh=6):
-    # if len(matches) < 8:
-    #     return None, None, 0
+    data = {
+        "keypoints0": k1[None],
+        "keypoints1": k2[None],
+        "descriptors0": d1[None],
+        "descriptors1": d2[None],
+        "scores0": s1[None],
+        "scores1": s2[None],
+        "image0": torch.empty((1,1,*img1.shape[:2]), device=device),
+        "image1": torch.empty((1,1,*img2.shape[:2]), device=device),
+    }
 
-    # src = np.float32([kp_d[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
-    # dst = np.float32([kp_s[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
+    with torch.no_grad():
+        pred = superglue(data)
 
-    # H, mask = cv2.findHomography(src, dst, cv2.RANSAC, thresh)
+    matches = pred["matches0"][0].cpu().numpy()
+    valid = matches > -1
 
-    # if mask is None:
-    #     return None, None, 0
-
-    # inliers = int(mask.sum())
-    # return H, mask.ravel(), inliers
-
-def drone_center_to_sat_pixel(H, tile_offset, drone_shape):
-    """
-    H: 3x3 homography (drone → tile)
-    tile_offset: (x0, y0)
-    drone_shape: (H, W)
-    """
-    h, w = drone_shape
-
-    # Drone center pixel (homogeneous)
-    pt_drone = np.array([[w/2, h/2, 1]]).T
-
-    # Map to satellite tile
-    pt_tile = H @ pt_drone
-    pt_tile /= pt_tile[2]
-
-    x_tile, y_tile = pt_tile[0,0], pt_tile[1,0]
-
-    # Map to full satellite image
-    x_sat = x_tile + tile_offset[0]
-    y_sat = y_tile + tile_offset[1]
-
-    return x_sat, y_sat
-
-
-
-def projected_to_latlon(x, y, src_crs):
-    """
-    x, y: projected coordinates (meters)
-    src_crs: CRS from GeoTIFF
-    """
-    transformer = Transformer.from_crs(
-        src_crs,
-        "EPSG:4326",
-        always_xy=True
+    return (
+        k1[valid].cpu().numpy(),
+        k2[matches[valid]].cpu().numpy()
     )
 
-    lon, lat = transformer.transform(x, y)
-    return lat, lon
+def visualize_matches(img1, pts1, img2, pts2, max_lines=50):
+    """
+    img1, img2 : BGR images
+    pts1, pts2 : (N,2) matched points
+    """
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
 
-def sat_pixel_to_gps(transform, x, y, src_crs):
-    x_proj, y_proj = xy(transform, y, x)
-    lat, lon = projected_to_latlon(x_proj, y_proj, src_crs)
-    return lat, lon
+    canvas = np.zeros((max(h1, h2), w1 + w2, 3), dtype=np.uint8)
+    canvas[:h1, :w1] = img1
+    canvas[:h2, w1:] = img2
 
-def geoposition_drone_image(
-    image_name,
-    H_store,
-    drone_imgs_proc,
-    geo_transform, src_crs
+    n = min(len(pts1), max_lines)
+
+    for i in range(n):
+        p1 = tuple(pts1[i].astype(int))
+        p2 = (int(pts2[i][0] + w1), int(pts2[i][1]))
+
+        color = tuple(np.random.randint(0,255,3).tolist())
+        cv2.circle(canvas, p1, 3, color, -1)
+        cv2.circle(canvas, p2, 3, color, -1)
+        cv2.line(canvas, p1, p2, color, 1)
+
+    return canvas
+
+def is_homography_valid(H, img_shape):
+    h, w = img_shape[:2]
+
+    corners = np.array([
+        [0, 0, 1],
+        [w, 0, 1],
+        [w, h, 1],
+        [0, h, 1]
+    ]).T
+
+    warped = H @ corners
+    warped /= warped[2]
+
+    xs = warped[0]
+    ys = warped[1]
+
+    # Reject if extreme distortion
+    if np.any(np.isnan(xs)) or np.any(np.isnan(ys)):
+        return False
+
+    area = cv2.contourArea(
+        np.vstack([xs, ys]).T.astype(np.float32)
+    )
+
+    if area < 0.05 * (w * h):
+        return False
+
+    if area > 10 * (w * h):
+        return False
+
+    return True
+
+def estimate_best_satellite_patch(
+    drone_img, sat_img,
+    window=512, stride=256
 ):
-    if image_name not in H_store:
+    best = None
+    best_inliers = 0
+    drone_img = cv2.resize(drone_img, (window, window))
+    for y in range(0, sat_img.shape[0]-window, stride):
+        for x in range(0, sat_img.shape[1]-window, stride):
+            patch = sat_img[y:y+window, x:x+window]
+
+            mk0, mk1 = superglue_match(drone_img, patch)
+            if len(mk0) < 12:
+                continue
+
+            H, mask = cv2.findHomography(mk0, mk1, cv2.RANSAC, 5.0)
+            # H, mask = cv2.findHomography(mk0, mk1, cv2.RANSAC, 5.0)
+            if H is None:
+                continue
+
+            if not is_homography_valid(H, drone_img.shape):
+                continue
+
+            inliers = int(mask.sum())
+            mask = mask.ravel().astype(bool)
+            if inliers > best_inliers:
+                best_inliers = inliers
+                best = {
+                    "H": H,
+                    "x": x,
+                    "y": y,
+                    "mk0":mk0,
+                    "mk1":mk1,
+                    "patch": patch,
+                    "inlier_mask":mask,
+                    "inliers": inliers
+                }
+
+    return best
+
+def save_best_inlier_npz(best, save_dir, image_id):
+    os.makedirs(save_dir, exist_ok=True)
+
+    np.savez(
+        os.path.join(save_dir, f"{image_id}_best_inlier.npz"),
+        H=best["H"],
+        x=best["x"],
+        y=best["y"],
+        patch=best["patch"],
+        mk0=best["mk0"],
+        mk1=best["mk1"],
+        inliersk=best["inliers"],
+        # num_inliers=best["num_inliers"]
+    )
+
+def ensure_dir(path):
+    os.makedirs(path, exist_ok=True)
+
+def pixel_to_latlon(px, py, transform, transformer):
+    # Pixel → projected coordinates
+    x_proj, y_proj = rasterio.transform.xy(
+        transform, py, px, offset="center"
+    )
+
+    # Projected → lat/lon
+    lon, lat = transformer.transform(x_proj, y_proj)
+    return lat, lon
+
+def estimate_gps_from_inliers(best, geo_transform, transformer):
+    """
+    Estimate GPS using centroid of satellite inlier points
+    """
+    mask = best["inlier_mask"]
+
+    sat_pts = best["mk1"][mask]
+
+    sat_pts = np.asarray(sat_pts)
+
+    if sat_pts.ndim == 3:
+        sat_pts = sat_pts.squeeze()
+
+    if sat_pts.ndim != 2 or sat_pts.shape[1] != 2:
+        print("Invalid sat_pts shape:", sat_pts.shape)
         return None
 
-    data = H_store[image_name]
-    H = data["H"]
-    offset = data["offset"]
+    if len(sat_pts) < 3:
+        return None
 
-    drone_img = drone_imgs_proc[image_name]
+    px = sat_pts[:, 0].mean() + best["x"]
+    py = sat_pts[:, 1].mean() + best["y"]
+
+    return pixel_to_latlon(px, py, geo_transform, transformer)
+
+def visualize_gps_on_satellite(i,
+    sat_img, px, py, patch_info=None, title="GPS estimate"
+):
+    plt.figure(figsize=(8,8))
+    plt.imshow(sat_img)
+    plt.scatter(px, py, c="red", s=60, marker="x", label="Estimated GPS")
+
+    if patch_info is not None:
+        x, y, w = patch_info
+        rect = plt.Rectangle(
+            (x, y), w, w,
+            edgecolor="lime", facecolor="none", linewidth=2,
+            label="Matched patch"
+        )
+        plt.gca().add_patch(rect)
+
+    plt.legend()
+    plt.title(title+f'_i')
+    plt.axis("off")
+    plt.show()
+
+def save_gps_on_satellite(i,
+    sat_img, px, py, best, out_dir
+):
+    ensure_dir(out_dir)
+
+    vis = sat_img.copy()
+
+    # GPS point
+    cv2.drawMarker(
+        vis,
+        (int(px), int(py)),
+        (0, 0, 255),
+        markerType=cv2.MARKER_CROSS,
+        markerSize=100,
+        thickness=10
+    )
+
+    # Patch bounding box
+    x, y = best["x"], best["y"]
+    h, w = best["patch"].shape[:2]
+    cv2.rectangle(
+        vis,
+        (x, y),
+        (x + w, y + h),
+        (0, 255, 0),
+        20
+    )
+
+    cv2.imwrite(os.path.join(out_dir, f"gps_on_satellite_{i}.png"), vis)
+
+def align_drone_to_satellite(drone_img, sat_img, path, i,vis = False):
+    window = 512
+    drone_img = cv2.resize(drone_img, (window, window))
+    best = estimate_best_satellite_patch(drone_img, sat_img)
+    image_id = os.path.splitext(os.path.basename(path))[0]
+    save_best_inlier_npz(best, "output_dir", image_id)
+    if best is None:
+        return None
+
     h, w = drone_img.shape[:2]
+    center = np.array([[w/2, h/2, 1]]).T
+    mapped = best["H"] @ center
+    mapped /= mapped[2]
 
-    x_sat, y_sat = drone_center_to_sat_pixel(
-        H, offset, (h, w)
+    px = mapped[0] + best["x"]
+    py = mapped[1] + best["y"]
+    # print("mk1 shape:", best["mk1"].shape)
+    # print("mask shape:", best["inlier_mask"].shape)
+    # Inlier satellite points (pixel space)
+    sat_inliers = best["mk1"][best["inlier_mask"]]
+
+    px = sat_inliers[:,0].mean() + best["x"]
+    py = sat_inliers[:,1].mean() + best["y"]
+
+    # px = sat_pts[:,0].mean() + best["x"]
+    # py = sat_pts[:,1].mean() + best["y"]
+
+    if vis: visualize_gps_on_satellite(i,
+        sat_img,
+        px, py,
+        patch_info=(best["x"], best["y"], best["patch"].shape[0]),
+        title="Estimated GPS location on satellite"
     )
-
-    # lat, lon = sat_pixel_to_gps(
-    #     geo_transform, x_sat, y_sat
-    # )
-
-    lat, lon = sat_pixel_to_gps(
+    out_dir = 'output'
+    save_gps_on_satellite(i,sat_img, px, py, best, out_dir)
+    lat, lon  = estimate_gps_from_inliers(
+        best,
         geo_transform,
-        x_sat,
-        y_sat,
-        src_crs
+        to_wgs84
     )
-    return {
-        "image": image_name,
-        "sat_x": x_sat,
-        "sat_y": y_sat,
-        "latitude": lat,
-        "longitude": lon,
-        "inliers": data["inliers"]
-    }
+    # return gps
+    # lat, lon = estimate_gps_from_inliers
+    # pixel_to_latlon(
+    # px, py,
+    # geo_transform,
+    # to_wgs84
+    # )
+    return lat, lon
 
-#Compare different feature extractor methods
 
-def match_orb(img1, img2):
-    orb = cv2.ORB_create(nfeatures=3000)
-
-    t0 = time.time()
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, None)
-
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches = bf.knnMatch(des1, des2, k=2)
-    good = ratio_test(matches, 0.75)
-    inliers = ransac_verify(kp1, kp2, good)
-    t = time.time() - t0
-
-    return {
-        "method": "ORB",
-        "keypoints": (len(kp1), len(kp2)),
-        "raw_matches": len(matches),
-        "good_matches": len(good),
-        "inliers": inliers,
-        "time": t
-    }
-
-def match_sift(img1, img2):
-    sift = cv2.SIFT_create(nfeatures=4000)
-
-    t0 = time.time()
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-
-    flann = cv2.FlannBasedMatcher(
-        dict(algorithm=1, trees=5),
-        dict(checks=50)
-    )
-
-    matches = flann.knnMatch(des1.astype(np.float32),
-                             des2.astype(np.float32), k=2)
-
-    good = ratio_test(matches, 0.85)
-    inliers = ransac_verify(kp1, kp2, good)
-    t = time.time() - t0
-
-    return {
-        "method": "SIFT",
-        "keypoints": (len(kp1), len(kp2)),
-        "raw_matches": len(matches),
-        "good_matches": len(good),
-        "inliers": inliers,
-        "time": t
-    }
-
-def match_surf(img1, img2):
-    surf = cv2.xfeatures2d.SURF_create(hessianThreshold=400)
-
-    t0 = time.time()
-    kp1, des1 = surf.detectAndCompute(img1, None)
-    kp2, des2 = surf.detectAndCompute(img2, None)
-
-    flann = cv2.FlannBasedMatcher(
-        dict(algorithm=1, trees=5),
-        dict(checks=50)
-    )
-
-    matches = flann.knnMatch(des1, des2, k=2)
-    good = ratio_test(matches, 0.8)
-    inliers = ransac_verify(kp1, kp2, good)
-    t = time.time() - t0
-
-    return {
-        "method": "SURF",
-        "keypoints": (len(kp1), len(kp2)),
-        "raw_matches": len(matches),
-        "good_matches": len(good),
-        "inliers": inliers,
-        "time": t
-    }
-
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371000
+    dlat = radians(lat2-lat1)
+    dlon = radians(lon2-lon1)
+    a = sin(dlat/2)**2 + cos(radians(lat1))*cos(radians(lat2))*sin(dlon/2)**2
+    return 2*R*atan2(sqrt(a), sqrt(1-a))
